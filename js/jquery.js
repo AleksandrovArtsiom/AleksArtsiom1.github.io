0e29e2b04cec8bf5734a692122b0e4b62a30e588
Mrzittack <zittack1@gmail.com>
2020-10-03 23:44:00 +0200

    initial commit

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+<script>
+  $(document).ready(function() {
+    $(".burger").click(function() {
+      $('.nav-links').toggleClass('active');
+    });
+  });
+  $(".close-icon").click(function() {
+    $(".nav-links").toggleClass("active");
+  });
+</script>
